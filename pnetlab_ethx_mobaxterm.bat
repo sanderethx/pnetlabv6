b2e860160c337848980251c204244d950eac55ef
sanderethx <61508739+sanderethx@users.noreply.github.com>
2024-03-05 16:14:44 -0300

    Create pnetlab_ethx_mobaxterm.bat

---
+++ pnetlab_ethx_mobaxterm.bat
@@ -0,0 +1,5 @@
+@echo off
+ SET input=%1
+ FOR /f "tokens=1,2,3 delims=:" %%a IN ("%input%") do SET host=%%b&SET port=%%c
+ SET host=%host:~2%
+ start "" /D "C:\Program Files (x86)\Mobatek\MobaXterm" MobaXterm.exe -newtab "telnet -r %host% %port%"
